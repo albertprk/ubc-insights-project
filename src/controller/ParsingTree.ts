7828fe9a399fb3e747e5490bc683adf14dd1bc62
p9i2b <madeleinemleroux@gmail.com>
2020-02-03 07:43:26 -0800

    Added lowercase comparison, guard for Nan and else for sorting

--- src/controller/ParsingTree.ts
+++ src/controller/ParsingTree.ts
@@ -181,10 +181,7 @@ export default class ParsingTree {
         }
     }
 
-    public reformatSection(
-        section: any,
-        columns: string[],
-    ): Record<string, any> {
+    public reformatSection(section: any, columns: string[]): Record<string, any> {
         let reformattedSection: Record<string, any> = {};
 
         try {
@@ -199,16 +196,12 @@ export default class ParsingTree {
                         key === "year" && section["Section"] === "overall"
                             ? 1900
                             : parseInt(section[this.MFIELD_MAP[key]], 10);
-                } else if (
-                    typeof section[this.MFIELD_MAP[key]] !== "undefined"
-                ) {
+                } else if (typeof section[this.MFIELD_MAP[key]] !== "undefined") {
                     reformattedSection[col] = section[this.MFIELD_MAP[key]];
-                } else if (
-                    typeof section[this.SFIELD_MAP[key]] !== "undefined"
-                ) {
+                } else if (typeof section[this.SFIELD_MAP[key]] !== "undefined") {
                     reformattedSection[col] =
                         key === "uuid"
-                            ? section[this.SFIELD_MAP[key]].toString()
+                            ? section[this.SFIELD_MAP[key]]
                             : section[this.SFIELD_MAP[key]];
                 } else {
                     return null;
@@ -225,21 +218,21 @@ export default class ParsingTree {
         if (typeof query["OPTIONS"]["ORDER"] !== "undefined") {
             orderKey = query["OPTIONS"]["ORDER"];
         } else {
-            orderKey = query["OPTIONS"]["COLUMNS"][0];
+            return sections;
         }
 
         sections.sort((a: any, b: any) => {
-            if (typeof a[orderKey] === "string" && a[orderKey] < b[orderKey]) {
+            if (typeof a[orderKey] === "string" && a[orderKey].toLowerCase() < b[orderKey].toLowerCase()) {
                 return -1;
-            } else if (
-                typeof a[orderKey] === "string" &&
-                a[orderKey] > b[orderKey]
-            ) {
+            } else if (typeof a[orderKey] === "string" && a[orderKey].toLowerCase() > b[orderKey].toLowerCase()) {
                 return 1;
             } else if (typeof a[orderKey] === "string") {
                 return 0;
+            } else if (typeof a[orderKey] === "number") {
+              let result: number = a[orderKey] - b[orderKey];
+              return isNaN(a[orderKey]) ? 1 : (isNaN(result)) ? -1 : result;
             } else {
-                return a[orderKey] - b[orderKey];
+                return 1;
             }
         });
 
