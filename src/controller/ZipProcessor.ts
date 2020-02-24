dfc9f13b4b34837f783dacba8f0090ca9853e6ea
Albert Park <albert.park@hotmail.com>
2020-02-24 11:26:23 -0800

    Refactored InsightFacade, and moved zip processing code into new class ZipProcessor

---
+++ src/controller/ZipProcessor.ts
@@ -0,0 +1,74 @@
+import {InsightDatasetKind, InsightError} from "./IInsightFacade";
+import * as JSZip from "jszip";
+import Dataset from "./Dataset";
+import Log from "../Util";
+
+
+export default class ZipProcessor {
+    public id: string;
+    public content: string;
+    public kind: InsightDatasetKind;
+
+    constructor(id: string, content: string, kind: InsightDatasetKind) {
+        this.id = id;
+        this.content = content;
+        this.kind = kind;
+    }
+
+    public processZipContent(): Promise<Dataset> {
+        return new Promise((resolve, reject) => {
+            let zipFile: JSZip = new JSZip();
+            zipFile.loadAsync(this.content, {base64: true}).then((files) => {
+                    let promises: Array<Promise<string>> = [];
+                    files.folder("courses").forEach((file) => {
+                        promises.push(
+                            files
+                                .folder("courses")
+                                .file(file)
+                                .async("text"),
+                        );
+                    });
+                    return Promise.all(promises);
+                }).then((sectionPromises: string[]) => {
+                    if (sectionPromises.length === 0) {
+                        reject(new InsightError("No courses files in dataset."));
+                    }
+
+                    let dataset: Dataset = new Dataset(this.id, this.kind);
+                    sectionPromises.forEach((sectionPromise: string) => {
+                        try {
+                            let jsonFile = JSON.parse(sectionPromise);
+                            let results = jsonFile["result"];
+                            for (let section of results) {
+                                if (this.isValidSection(section)) {
+                                    dataset.addSection(section);
+                                }
+                            }
+                        } catch (err) {
+                            Log.info("Caught invalid JSON file");
+                        }
+                    });
+
+                    if (dataset.sections.length === 0) {
+                        reject(new InsightError("No valid course sections"));
+                    }
+                    resolve(dataset);
+                });
+        });
+    }
+
+    public processRoomsZipContent(id: string, content: string, kind: InsightDatasetKind): Promise<string> {
+        return Promise.resolve(null);
+    }
+
+    private isValidSection(section: any): boolean {
+        const keys: string[] = ["Avg", "Audit", "Fail", "Pass", "Subject", "Course",
+            "Professor", "Title", "id"];
+        for (let key of keys) {
+            if (typeof section[key] === "undefined") {
+                return false;
+            }
+        }
+        return true;
+    }
+}
